8cbcabb0843c5d6df9953b3edee856170d5d5fac
Patrick Coxall <mr.coxall@mths.ca>
2023-08-23 16:19:34 +0000

    working python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,12 +5,44 @@ This module does basic math
 """
 
 from microbit import *
+from time import sleep
 
 
-sleep(1000)
+display.clear()
+sleep(1)
 
 # addition
-display.scroll('40 + 2 = ' + (40 + 2))
-display.show(Image.Happy)
-sleep(100)
+display.scroll('40 + 2 = ' + str(40 + 2))
+display.show(Image.HAPPY)
+sleep(0.5)
+display.clear()
+
+# subtraction
+display.scroll('45 - 3 = ' + str(45 - 3))
+display.show(Image.HAPPY)
+sleep(0.5)
+display.clear()
+
+# multiplication
+display.scroll('2 + 8 x 5 = ' + str(2 + 8 * 5))
+display.show(Image.HAPPY)
+sleep(0.5)
+display.clear()
+
+# division
+display.scroll('(80 / 2) + 2  = ' + str((80 / 2) + 2))
+display.show(Image.HAPPY)
+sleep(0.5)
+display.clear()
+
+# incorrect way to do exponents
+display.scroll('10 + 2^5 = ' + str(10 + 2^5))
+display.show(Image.SAD)
+sleep(0.5)
+display.clear()
+
+# exponent
+display.scroll('10 + 2^5 = ' + str(10 + 2**5))
+display.show(Image.HAPPY)
+sleep(0.5)
 display.clear()
---
+++ MicroPython/test.py
@@ -0,0 +1,18 @@
+"""
+Created by: Mr. Coxall
+Created on: Sep 2020
+This module does basic math
+"""
+
+#from microbit import *
+from time import sleep
+
+
+sleep(1)
+
+# addition
+print('40 + 2 = ' + str(40 + 2))
+
+#display.show(Image.Happy)
+sleep(0.1)
+#display.clear()
