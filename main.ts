82d174784812a4fbb7c5e8994fa150e6cf43f6e2
Patrick Coxall <mr.coxall@mths.ca>
2023-08-23 11:42:34 -0400

    convert number to string for linting fix

--- main.ts
+++ main.ts
@@ -8,31 +8,31 @@
 basic.pause(1000)
 
 // addition
-basic.showString('40 + 2 = ' + (40 + 2))
+basic.showString('40 + 2 = ' + (40 + 2).toString())
 basic.showIcon(IconNames.Happy)
 basic.pause(100)
 basic.clearScreen()
 
 // subtraction
-basic.showString('45 - 3 = ' + (45 - 3))
+basic.showString('45 - 3 = ' + (45 - 3).toString())
 basic.showIcon(IconNames.Happy)
 basic.pause(100)
 basic.clearScreen()
 
 // multiply
-basic.showString('2 + 8 x 5 = ' + (2 + 8 * 5))
+basic.showString('2 + 8 x 5 = ' + (2 + 8 * 5).toString())
 basic.showIcon(IconNames.Happy)
 basic.pause(100)
 basic.clearScreen()
 
 // divide
-basic.showString('(80 / 2) + 2 = ' + (80 / 2 + 2))
+basic.showString('(80 / 2) + 2 = ' + (80 / 2 + 2).toString())
 basic.showIcon(IconNames.Happy)
 basic.pause(100)
 basic.clearScreen()
 
 // exponent
-basic.showString('10 + 2^5 = ' + (10 + 2 ** 5))
+basic.showString('10 + 2^5 = ' + (10 + 2 ** 5).toString())
 basic.showIcon(IconNames.Happy)
 basic.pause(100)
 basic.clearScreen()
